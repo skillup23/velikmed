94e278fac0b53d250162b760a2e6ff92aea27dab
skillup23 <skillup23@yandex.ru>
2025-02-22 17:31:48 +0300

    docker 0.2

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// export default nextConfig;
