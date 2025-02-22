5bbba8cd08c82638fce1ed7e8d917a393c92936f
skillup23 <skillup23@yandex.ru>
2025-02-22 18:00:03 +0300

    nodocker 0.3

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+// };
+
+// export default nextConfig;
