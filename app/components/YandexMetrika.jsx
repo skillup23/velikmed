22af6af92ddf6693dc2ee44c576efb26bd2cfb5d
skillup23 <skillup23@yandex.ru>
2025-12-03 18:56:47 +0300

    метрика v10

--- app/components/YandexMetrika.jsx
+++ app/components/YandexMetrika.jsx
@@ -1,38 +1,54 @@
-'use client';
-import Script from 'next/script';
+"use client";
 
-export default function YandexMetrika() {
+import { useEffect } from "react";
+
+const YandexMetrika = () => {
+  useEffect(() => {
+    // Проверяем, что скрипт ещё не загружен
+    if (window.ym) return;
+
+    // Вставляем скрипт Метрики
+    (function (m, e, t, r, i) {
+      m[i] =
+        m[i] ||
+        function () {
+          (m[i].a = m[i].a || []).push(arguments);
+        };
+      m[i].l = 1 * new Date();
+      const k = e.createElement(t);
+      const a = e.getElementsByTagName(t)[0];
+      k.async = 1;
+      k.src = r;
+      a?.parentNode?.insertBefore(k, a);
+    })(
+      window,
+      document,
+      "script",
+      "https://mc.yandex.ru/metrika/tag.js?id=105637836",
+      "ym"
+    );
+
+    // Инициализируем счётчик
+    window.ym(105637836, "init", {
+      ssr: true,
+      webvisor: true,
+      clickmap: true,
+      ecommerce: "dataLayer",
+      accurateTrackBounce: true,
+      trackLinks: true,
+    });
+  }, []);
+
   return (
     <>
-      <Script
-        id="yandex-metrika"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(105637836, "init", {
-              ssr: true,
-              webvisor: true,
-              clickmap: true,
-              ecommerce: "dataLayer",
-              accurateTrackBounce: true,
-              trackLinks: true
-            });
-          `,
-        }}
-      />
+      {/* Ноускрипт-версия */}
       <noscript>
-        <div>
-          <img
-            src="https://mc.yandex.ru/watch/105637836"
-            style={{ position: 'absolute', left: '-9999px' }}
-            alt=""
-          />
+        <div style={{ position: "absolute", left: "-9999px" }}>
+          <img src="https://mc.yandex.ru/watch/105637836" alt="" />
         </div>
       </noscript>
     </>
   );
-}
+};
+
+export default YandexMetrika;
--- app/layout.js
+++ app/layout.js
@@ -1,37 +1,34 @@
-import { Roboto } from 'next/font/google';
-import localFont from 'next/font/local';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import './globals.css';
-// import Script from 'next/script';
-import YandexMetrika from './components/YandexMetrika';
-// import { YandexMetricaProvider } from 'next-yandex-metrica';
-// import MetrikaDebug from './components/MetrikaDebug';
+import { Roboto } from "next/font/google";
+import localFont from "next/font/local";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import YandexMetrika from "./components/YandexMetrika";
+import "./globals.css";
 
 const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto',
+  weight: ["300", "400", "500", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
 });
 
 const Manrope = localFont({
   src: [
     {
-      path: './fonts/manrope/manrope-bold.woff2',
-      weight: '700',
-      style: 'bold',
+      path: "./fonts/manrope/manrope-bold.woff2",
+      weight: "700",
+      style: "bold",
     },
     {
-      path: './fonts/manrope/manrope-semibold.woff2',
-      weight: '600',
-      style: 'semibold',
+      path: "./fonts/manrope/manrope-semibold.woff2",
+      weight: "600",
+      style: "semibold",
     },
     {
-      path: './fonts/manrope/manrope-medium.woff2',
-      weight: '500',
-      style: 'medium',
+      path: "./fonts/manrope/manrope-medium.woff2",
+      weight: "500",
+      style: "medium",
     },
   ],
 });
@@ -39,21 +36,21 @@ const Manrope = localFont({
 const MAK = localFont({
   src: [
     {
-      path: './fonts/mak/MAK.woff',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/mak/MAK.woff",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/mak/MAK-bold.woff',
-      weight: '600',
-      style: 'bold',
+      path: "./fonts/mak/MAK-bold.woff",
+      weight: "600",
+      style: "bold",
     },
   ],
 });
 
 export const metadata = {
-  title: 'Клиника Великмед',
-  description: 'Официальный сайт клиники Великмед',
+  title: "Клиника Великмед",
+  description: "Официальный сайт клиники Великмед",
 };
 
 export default function RootLayout({ children }) {
@@ -68,19 +65,8 @@ export default function RootLayout({ children }) {
       <body>
         <Header />
         {children}
-        <Footer />
         <YandexMetrika />
-        {/* <YandexMetrika /> */}
-        {/* <MetrikaDebug /> Временно для отладки */}
-        {/* <noscript>
-          <div>
-            <img
-              src={`https://mc.yandex.ru/watch/105637836`}
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
-            />
-          </div>
-        </noscript> */}
+        <Footer />
       </body>
     </html>
   );
