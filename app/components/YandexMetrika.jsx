b93490bf682337f6ea5f1ed5f6d99f40a33d1022
Robert Anisimov <skillup23@yandex.ru>
2025-12-03 17:17:35 +0300

    яндекс метрика v4

--- app/components/YandexMetrika.jsx
+++ app/components/YandexMetrika.jsx
@@ -1,23 +1,23 @@
-'use client';
+// 'use client';
 
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
-export default function YandexMetrika() {
-  useEffect(() => {
-    // Вставляем оригинальный код Яндекс.Метрики
-    const script = document.createElement('script');
-    script.innerHTML = `
-      (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105637836', 'ym');
+// export default function YandexMetrika() {
+//   useEffect(() => {
+//     // Вставляем оригинальный код Яндекс.Метрики
+//     const script = document.createElement('script');
+//     script.innerHTML = `
+//       (function(m,e,t,r,i,k,a){
+//         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+//         m[i].l=1*new Date();
+//         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+//         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+//       })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105637836', 'ym');
 
-      ym(105637836, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-    `;
-    document.head.appendChild(script);
-  }, []);
+//       ym(105637836, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+//     `;
+//     document.head.appendChild(script);
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
--- app/layout.js
+++ app/layout.js
@@ -66,17 +66,17 @@ export default function RootLayout({ children }) {
         <Script
           id="yandex-metrika"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=105637836', 'ym');
-
-      ym(105637836, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-    `,
+          src="https://mc.yandex.ru/metrika/tag.js?id=105637836"
+          onLoad={() => {
+            // Инициализация после загрузки скрипта
+            window.ym(105637836, 'init', {
+              ssr: true,
+              webvisor: true,
+              clickmap: true,
+              ecommerce: 'dataLayer',
+              accurateTrackBounce: true,
+              trackLinks: true,
+            });
           }}
         />
       </head>
